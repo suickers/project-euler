6f46469bacbec0ad4812fb279aea6a0e38c0bf2a
suickers <pearsonjames1120@gmail.com>
2025-11-06 18:23:08 +0000

    added a memo but it's now slower than before...

--- src/bin/PE14.rs
+++ src/bin/PE14.rs
@@ -1,16 +1,23 @@
-// naive recursive approach. trying to think how to memoize it. 
+use std::collections::HashMap;
 
-fn length(n: usize, count: usize) -> usize {
-	if n == 1 { return count; }
-	length(if n % 2 == 0 { n / 2 } else { (3*n) + 1 }, count + 1)
+fn length(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
+	if let Some(&v) = memo.get(&n) {
+		return v; 
+	}
+	let next = if n % 2 == 0 { n / 2 } else { (3*n) + 1 };
+	let v = 1 + length(next, memo);
+	memo.insert(n, v);
+	v
 }
 
 fn main() {
 	let mut max_n = 0;
 	let mut max_s = 0;
-
+	let mut memo = HashMap::new();
+	memo.insert(1, 0);
+	
 	for i in 1..1_000_000 {
-		let s = length(i, 1);
+		let s = length(i, &mut memo);
 
 		if s > max_s {
 			max_s = s;
