a44a0855797615972445b09d915ff39789522124
suickers <pearsonjames1120@gmail.com>
2025-11-07 11:19:43 +0000

    use is_prime from lib.rs in PE07

--- src/bin/PE07.rs
+++ src/bin/PE07.rs
@@ -1,17 +1,8 @@
-fn is_prime(n: u64) -> bool {
-	if n < 2 { return false; }
-	if n % 2 == 0 { return n == 2; }
-
-  // i was messing around with Rust's ranges here
-	(3u64..)
-		.step_by(2)
-		.take_while(|&x| x <= n / x )
-		.all(|x| n % x != 0)
-}
+use project_euler::primes::is_prime;
 
 fn main() {
 	let mut primes = (2u64..)
 		.filter(|x| is_prime(*x));
-  // unwrap because .nth() returns an Option 
-	println!("{}", primes.nth(10000).unwrap());
+
+	assert_eq!(primes.nth(10000).unwrap(), 104743);
 }
