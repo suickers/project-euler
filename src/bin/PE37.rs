73fa00ff33d1b50b9f9c12d498da7afd6826ef08
suickers <pearsonjames1120@gmail.com>
2025-11-15 17:17:51 +0000

    finished PE37

--- src/bin/PE37.rs
+++ src/bin/PE37.rs
@@ -1,15 +1,37 @@
-fn truncate(mut n: usize) -> Vec<usize> {
-	let len = n.ilog10() + 1; 
+use project_euler::primes::sieve;
+use std::time::Instant;
 
+fn truncate(n: usize) -> Vec<usize> {
+	let len = n.ilog10() + 1; 
+	let mut en = n;
 	let mut vec = Vec::new(); 
 
 	for _ in 1..=len {
-		vec.push(n);
-		n /= 10;
+		vec.push(en);
+		en /= 10;
 	}	
+	en = n;
+
+	for i in (0..len).rev() {
+		vec.push(en);
+		en %= 10usize.pow(i);
+	}
 	vec
+	
 }
 
 fn main() {
-	println!("{:?}", truncate(3797));
+	let time = Instant::now();
+	
+	let mut sum = 0;
+	let limit = 10usize.pow(6);
+	let sieve = sieve(limit);
+	for i in 8..=limit {
+		if truncate(i).into_iter().all(|x| sieve[x]) {
+			sum += i;
+		}
+	}
+
+	println!("{}", sum);
+	println!("{:?}", time.elapsed());
 }
