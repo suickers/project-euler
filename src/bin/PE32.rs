0403f4758d3e11640424331d5dd03837ec8b2e1c
James Pearson <92632267+suickers@users.noreply.github.com>
2025-11-20 12:08:16 +0000

    Optimisations to pe 32

--- src/bin/PE32.rs
+++ src/bin/PE32.rs
@@ -3,40 +3,43 @@ use std::time::Instant;
 use std::iter::successors; 
 
 fn is_pandigital_product(m: usize, a: usize, p: usize) -> bool {
-    let mut vec = vec![true; 10];
-    vec[0] = false;
-    
+    let mut arr = [true; 10];
+    arr[0] = false;
+
     if (m.ilog10() + 1) + (a.ilog10() + 1) + (p.ilog10() + 1) != 9 {
         return false;
     }
-    
-    for n in digits_of(m) {
-        vec[n] = false;
+
+    let x = concat(m, a, p);
+
+    for n in digits_of(x) {
+        arr[n] = false;
     }
-    for n in digits_of(a) {
-        vec[n] = false;
-    }
-    for n in digits_of(p) {
-        vec[n] = false;
-    }
-    
-    vec == vec![false; 10]
+
+    arr == [false; 10]
 }
 
-fn digits_of(n: usize) -> impl Iterator<Item = usize> {
-    successors(Some(n), |x| {
+fn digits_of(a: usize) -> impl Iterator<Item = usize> {
+    successors(Some(a), |x| {
         if *x >= 10 { Some(x / 10) } else { None }
     })
     .map(|x| x % 10)
 }
 
+fn concat(a: usize, b: usize, c: usize) -> usize {
+    let n1 = b.ilog10() + 1;
+    let n2 = c.ilog10() + 1;
+
+    (a * 10usize.pow(n1) + b) * 10usize.pow(n2) + c
+}
+
 fn main() {
-    
+
     let time = Instant::now();
-    
-    let mut set = HashSet::new(); 
-    for i in 1..=99 {
-        for j in 1..=2000 {
+    let mut set = HashSet::new();
+
+    for i in 1..=87 {
+        for j in 1..=1965 {
             let product = i*j;
             if is_pandigital_product(i, j, product) {
                 set.insert(product);
@@ -44,8 +47,8 @@ fn main() {
         }
     }
     let sum: usize = set.into_iter().sum();
-
     assert_eq!(sum, 45228);
-    
-    println!("{:?}", time.elapsed());
+
+    // 891.074µs on Rust Playground 
+    println!("{:?}", time.elapsed()); 
 }
