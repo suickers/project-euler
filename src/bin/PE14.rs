dc7f57bb6691f6043b8f81d518e46c4e8ba8cf41
James Pearson <92632267+suickers@users.noreply.github.com>
2025-11-21 15:01:21 +0000

    Used a vector instead of hashmap for pe14 memo

--- src/bin/PE14.rs
+++ src/bin/PE14.rs
@@ -1,22 +1,39 @@
-use std::collections::HashMap;
+use std::time::Instant;
 
-fn length(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
-	if let Some(&v) = memo.get(&n) {
-		return v; 
+fn length(n: usize, memo: &mut Vec<Option<usize>>) -> usize {
+	
+	let memlen = memo.len();
+	
+	if n == 1 {
+	    return 0;
 	}
+	
+	if n < memlen {
+	    if let Some(v) = memo[n] {
+    		return v; 
+    	}
+	}
+	
 	let next = if n % 2 == 0 { n / 2 } else { (3*n) + 1 };
+	
 	let v = 1 + length(next, memo);
-	memo.insert(n, v);
+	
+	if n < memlen {
+	    memo[n] = Some(v);
+	}
+	
 	v
 }
 
 fn main() {
+    let time = Instant::now();
+    let lim = 1_000_000;
+    
 	let mut max_n = 0;
 	let mut max_s = 0;
-	let mut memo = HashMap::new();
-	memo.insert(1, 0);
+	let mut memo = vec![None; lim + 1];
 	
-	for i in 1..1_000_000 {
+	for i in 1..lim {
 		let s = length(i, &mut memo);
 
 		if s > max_s {
@@ -24,5 +41,12 @@ fn main() {
 			max_n = i;
 		}
 	}
-	println!("{}", max_n);
+	assert_eq!(max_n, 837799);
+	
+	
+	
+	println!("{:?}", time.elapsed());
+	// ~ 384.100087ms with hashmap and no insert check
+	// ~ 248.277903ms with hashmap and n < 1mil check
+	// ~ 29.500366ms with vector memo 
 }
