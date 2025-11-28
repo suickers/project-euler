f24a19429ac3958dddb835ca87cce51b9c38b836
James Pearson <92632267+suickers@users.noreply.github.com>
2025-11-28 08:40:06 +0000

    Iter methods

--- src/bin/PE09.rs
+++ src/bin/PE09.rs
@@ -1,22 +1,30 @@
 fn solution(a: isize) -> isize {
 
-    let p = a / 2;
-    let mut m = 0;
-    let mut n = 0;
+    // let p = a / 2;
+    // let mut m = 0;
+    // let mut n = 0;
     
-    for em in 1..p {
-        for en in 1..em-1 {
-            if p % (em*(em+en)) == 0 {
-                (m, n) = (em, en);
-            }
-        }
-    }
+    // for em in 1..p {
+    //     for en in 1..em-1 {
+    //         if p % (em*(em+en)) == 0 {
+    //             (m, n) = (em, en);
+    //         }
+    //     }
+    // }
     
-    (2*m*n)*(m.pow(4)-n.pow(4))
+    // (2*m*n)*(m.pow(4)-n.pow(4))
+    
+    (1..a/2)
+        .flat_map(|em| {
+            (1..em-1)
+                .map(move |en| (em, en))
+                .filter(|(em, en)| (a/2) % (em*(em+en)) == 0)
+        })
+        .next()
+        .map(|(m, n)| (2*m*n)*(m.pow(4)-n.pow(4)))
+        .unwrap_or(0)
 }
 
 fn main() {
     assert_eq!(31875000, solution(1000));
 }
-
-    
